b2c74435134f113ba06bff6b37e8e9e907600573
selvakumar4122 <35727614+selvakumar4122@users.noreply.github.com>
2018-02-20 16:57:47 +0530

    Create count the number of letters.c

---
+++ count the number of letters.c
@@ -0,0 +1,15 @@
+ #include<stdio.h>
+ #include<string.h>
+ void main()
+    {
+    char s[20];
+    int n=1,i;
+    printf("enter the string :");
+    scanf("%[\n]s",s);
+    for(i=0;i<20;i++)
+   {
+       if(s[i]!='\t'||s[i]!='\0')
+    n++;
+   }
+    printf("The numbers in sendence : %d",n);
+    }
