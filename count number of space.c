9ff425bfc24964db59c26c06ffd777a7e3f4103e
selvakumar4122 <35727614+selvakumar4122@users.noreply.github.com>
2018-02-22 16:09:09 +0530

    Create count number of space.c

---
+++ count number of space.c
@@ -0,0 +1,15 @@
+ #include<stdio.h>
+ #include<string.h>
+ void main()
+    {
+    char s[20];
+    int n=0,i;
+    printf("enter the string :");
+    scanf("%[^\n]s",s);
+    for(i=0;s[i]!='\0';i++)
+   {
+       if(s[i]==' ')
+       n++;
+   }
+    printf("The numbers in sendence : %d",n);
+    }
