35f464bfcc527623e095ea253a2b717ddae7db12
selvakumar4122 <35727614+selvakumar4122@users.noreply.github.com>
2018-03-01 16:09:15 +0530

    Create swap by bit wise ope.c

---
+++ swap by bit wise ope.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+  int a,b,c;
+  printf("Enter the value of a:");
+    scanf("%d",&a);
+  printf("Enter the value of b:");
+    scanf("%d",&b);
+     a=a^b;
+     b=a^b;
+     a=a^b;
+  printf("The values after swap: %d %d",a,b);
+    }
