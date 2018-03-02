f7700ddb82853a829a968d622ba89f27ba3b6db0
selvakumar4122 <35727614+selvakumar4122@users.noreply.github.com>
2018-03-02 14:15:06 +0530

    Update counting of special character.c

--- counting of special character.c
+++ counting of special character.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
- #include<string.h>
- void main()
-    {
+#include<string.h>
+void main()
+{
     char s[20];
     int n=0,i,c=0;
     printf("enter the string :");
     scanf("%[^\n]s",s);
     for(i=0;s[i]!='\0';i++)
-   {
-       if(s[i]>=0||s[i]<=9||s[i]>='A'||s[i]<="z")
+     {
+       if((s[i]>=0&&s[i]<=9)||(s[i]>='A'&&s[i]<="z"))
        c++;
        else
        n++;
-   }
-    printf("The space in sentence : %d",n);
-    }
+     }
+    printf("The special charactor in sentence : %d",n);
+}
+
+
