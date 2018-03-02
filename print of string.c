cc64b1475c8bf466b67170931ca60259fe51c6f4
selvakumar4122 <35727614+selvakumar4122@users.noreply.github.com>
2018-03-02 14:26:30 +0530

    Create print of string.c

---
+++ print of string.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char s[20];
+    int n=0,i,c=0;
+    printf("Enter the string :");
+    scanf("%s",&s);
+    printf("Enter the value of n :");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    printf("%s\n",s);
+}
+
+
