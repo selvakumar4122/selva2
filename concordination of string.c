e521304196ce3bd2032e3343e0876a3598a0e0a0
selvakumar4122 <35727614+selvakumar4122@users.noreply.github.com>
2018-03-13 16:21:37 +0530

    Create concordination of string.c

---
+++ concordination of string.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+	char str1[50],str2[50];
+	printf("enter the string1:");
+	scanf("%s",str1);
+	printf("enter the string2:");
+	scanf("%s",str2);
+	printf("%s%s",str1,str2);
+}
