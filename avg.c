42a386508aac661751ec7fc4fab56c98f7d3ede9
selvakumar4122 <35727614+selvakumar4122@users.noreply.github.com>
2018-03-13 16:58:00 +0530

    Create avg.c

---
+++ avg.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+  float a[100000],t=0,avg;
+  int n,i;
+  printf("enter value for n:");
+  scanf("%d",&n);
+  printf("Enter the value for array : ");
+  for(i=0;i<n;i++)
+  scanf("%d",&a[i]);
+  for(i=0;i<n;i++)
+  {
+      t=t+a[i];
+  }
+  avg=t/n;
+  printf("The average is %f",avg);
+}
