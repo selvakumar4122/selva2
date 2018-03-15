011fee909653d056a44063551466e9652cb8d579
selvakumar4122 <35727614+selvakumar4122@users.noreply.github.com>
2018-03-15 15:44:42 +0530

    Update avg.c

--- avg.c
+++ avg.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 void main()
 {
-  float a[100000],t=0,avg;
+  int a[100000],t=0,avg;
   int n,i;
   printf("enter value for n:");
   scanf("%d",&n);
   printf("Enter the value for array : ");
   for(i=0;i<n;i++)
-  scanf("%f",&a[i]);
+  scanf("%d",&a[i]);
   for(i=0;i<n;i++)
   {
       t=t+a[i];
   }
   avg=t/n;
-  printf("The average is %f",avg);
+  printf("The average is %d",avg);
 }
